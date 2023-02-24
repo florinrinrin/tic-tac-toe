b1c0835a33094e680488a57d42a0e41c6cd9d179
Florin <User@192-168-0-103.rdsnet.ro>
2023-02-24 19:45:31 +0200

    ticktactoe

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("X ");
+        System.out.print("O ");
+        System.out.println("O ");
+        System.out.print("X ");
+        System.out.print("O ");
+        System.out.println("X ");
+        System.out.print("X ");
+        System.out.print("O ");
+        System.out.print("O ");
+
+        System.out.printf("%nRound Two:%nX O X%nO X O%nX X O%n");
+
+    }
+}
