572140ee7c559129c599f01d7babe5f1f004bf84
Florin <User@192-168-0-102.rdsnet.ro>
2023-03-04 12:26:43 +0200

    solved second tic-tac-toe task

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("X ");
-        System.out.print("O ");
-        System.out.println("O ");
-        System.out.print("X ");
-        System.out.print("O ");
-        System.out.println("X ");
-        System.out.print("X ");
-        System.out.print("O ");
-        System.out.print("O ");
-
-        System.out.printf("%nRound Two:%nX O X%nO X O%nX X O%n");
+        String userInput = scanner.next();
+        ticTacToe(userInput);
 
     }
+
+
+    public static void ticTacToe(String userInput) {
+
+        String firstLine = "";
+        String secondLine = "";
+        String thirdLine = "";
+        //System.out.print("---------");
+
+        for (int i = 0; i <= userInput.length(); i++) {
+            firstLine = userInput.substring(0, 3).replace(""," ").trim();
+            secondLine = userInput.substring(3, 6).replace(""," ").trim();
+            thirdLine = userInput.substring(6, 9).replace(""," ").trim();
+        }
+        System.out.println("---------" + custom(firstLine) + custom(secondLine) + custom(thirdLine) + "\n" + "---------");
+        //System.out.println("---------");
+    }
+
+    public static String custom (String line){
+        return "\n" + "| " + line + " |";
+    }
+
+
 }
